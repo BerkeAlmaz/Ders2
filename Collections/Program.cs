bb15c7721ac412854fc88c80ebdae9a64261ede6
Berke <berkealm@gmail.com>
2021-01-20 23:51:54 +0300

    20.01

---
+++ Collections/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Engin", "Berke", "Burxu", "Kuzmir" };
+            //Console.WriteLine(isimler[0]);
+            //isimler[4] = "x";
+            //Console.WriteLine(isimler[4]);
+
+            List<string> isimler2 = new List<string> { "Engin", "Berke", "Burxu", "Kuzmir" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Kerikuzi");
+            Console.WriteLine(isimler2[4]);
+            
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Kalem";
+            product1.UnitPrice = 150;
+            product1.UnitsInStock = 10;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, UnitsInStock = 23, ProductName = "Kol", UnitPrice = 450};
+
+            //PascalCase   //camelCase
+            //case sensitive
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            //int, double, bool... değer tip
+            //diziler, class, abstract class, interface... referans tip
+
+            
+        }
+    }
+}
